cba1e743f4b85d5945e756b52ed5440401696147
Eric Wohltman <eric.wohltman@gmail.com>
2020-02-19 00:53:08 -0500

    Add Kubernetes deployment strategy

---
+++ scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail # Only exit with zero if all commands of the pipeline exit successfully
+
+SCRIPT_PATH=$(readlink -f "${0}")
+SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+
+COMMIT=$(git rev-parse --short HEAD)
+
+REPO_YMLS="${SCRIPT_DIR}/../deployments/kubernetes"
+
+NAMESPACE_YML="${REPO_YMLS}/namespace.yml"
+SERVICE_YML="${REPO_YMLS}/service.yml"
+
+DEPLOYMENT_YML="${REPO_YMLS}/statefulset.yml"
+VARIABLIZED_DEPLOYMENT_YML="/tmp/statefulset.yml"
+
+setup() {
+  cp "${DEPLOYMENT_YML}" "${VARIABLIZED_DEPLOYMENT_YML}"
+}
+
+applyValues() {
+  sed -i "s|{COMMIT}|${COMMIT}|g" "${VARIABLIZED_DEPLOYMENT_YML}"
+}
+
+deploy() {
+  kubectl apply -f "${NAMESPACE_YML}"
+  kubectl apply -f "${SERVICE_YML}"
+  kubectl apply -f "${VARIABLIZED_DEPLOYMENT_YML}"
+  kubectl -n ephemeral-roles rollout status --timeout 120s deployment/pod-bouncer
+}
+
+cleanup() {
+  rm -f "${VARIABLIZED_DEPLOYMENT_YML}"
+}
+
+trap cleanup EXIT
+
+setup
+applyValues
+deploy
